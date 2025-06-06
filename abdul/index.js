a98648c2c4dd5ed42610c9bc3c750a96ad12d24f
NairobiCodeSculptor <abdulrahmankitiavi@gmail.com>
2025-06-06 09:33:44 +0300

    first commit

---
+++ abdul/index.js
@@ -0,0 +1,2 @@
+$("button").om("click" , function(){ 
+   $("") );
---
+++ index.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    
+    $("#what").click(function(){
+
+        $("#hide").slideToggle("slow");
+
+        $("#hide2").slideToggle("slow");
+
+      });
+   
+  });
